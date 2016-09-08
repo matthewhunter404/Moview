0b78a811f887814ec350f5192cb5e7aaf5b5a380
Matthew <matthewhunter404@gmail.com>
2016-09-08 23:11:18 +0300

    Adds a gridview to the main layout. Adds a gridview image layout for each individual image. Attempts to use an array adapter without using a custom gridadapter class, but this is not an option using an array adapter.

--- app/src/main/java/matt/moview/MainActivity.java
+++ app/src/main/java/matt/moview/MainActivity.java
@@ -2,12 +2,34 @@ package matt.moview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Integer[] values = new Integer[] {R.drawable.avengers, R.drawable.darkknight, R.drawable.maleficent,
+                R.drawable.manofsteel, R.drawable.skyfall, R.drawable.starwars, R.drawable.thegrey, R.drawable.titanic,
+                R.drawable.zootopia};
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,
+                R.layout.grid_image, values);
+
         setContentView(R.layout.activity_main);
+        GridView gridview = (GridView) findViewById(R.id.moviegridview);
+        gridview.setAdapter(adapter);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
